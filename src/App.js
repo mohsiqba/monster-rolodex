242ec1c8d25e0032d032a9069164d5fc25186b0a
mohsin iqbal <mohsin.iqbal@walmartlabs.com>
2019-08-24 00:41:12 +0530

    sample react app- monster-rolodex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
+import {CardList} from './component/card-list/card-list.component';
+import {SearchBox} from './component/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField:''
+    }
+  }
+
+  handleChange=e=>{
+    this.setState({searchField:e.target.value});
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+  render(){
+    const {monsters,searchField} = this.state;
+    const filteredMonsters=monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return <div className="App">
+      <h1> Monster Rolodex</h1>
+      <SearchBox placeholder='Search here !!' handleChange={this.handleChange} />
+      <CardList monsters={filteredMonsters} />
+  </div>
+  }
 }
+
 
 export default App;
---
+++ src/component/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = props => {
+    return <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set4&size=160x160`} alt='monster'/>
+        <h2> {props.monster.name} </h2>
+        <p>{props.monster.email}</p>
+    </div>
+
+}
