8e6471bb1beb36bd2950d997fc03889a55c73d85
mohsin iqbal <mohsin.iqbal@walmartlabs.com>
2019-08-24 00:39:08 +0530

    Updates

---
+++ precache-manifest.07044336005ed8dd5f3b96054bf86c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e5f6b8bee579848f28e2cffde4dc6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cc86a93a1e319a9294ad",
+    "url": "/monster-rolodex/static/css/main.21118cd6.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monster-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "cc86a93a1e319a9294ad",
+    "url": "/monster-rolodex/static/js/main.4aa49e3e.chunk.js"
+  },
+  {
+    "revision": "2a0841746d3b50a5d128",
+    "url": "/monster-rolodex/static/js/runtime~main.cc3a76ac.js"
+  }
+]);
